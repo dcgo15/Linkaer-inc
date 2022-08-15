8548eeb5081e974ec4287683f5e516f1620e2430
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-08-15 15:16:19 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 __author__ = "Daniel Gomes"
 
 
+
 from flask import Flask,request, render_template
+from pytube import YouTube
 import pyshorteners
 
 
@@ -9,20 +11,29 @@ NOME = "LINKAER"
 
 app = Flask(__name__)
 
-@app.route("/insta", methods=["GET", "POST"])
+
+@app.route("/yt", methods=["GET", "POST"])
 def download():
     if request.method == "POST":
         link_video = request.form.get("video")
-        return "Seu link é: " + link_video
+        yt = YouTube(link_video)
+        yt.streams.get_highest_resolution().download()
+        
+        
+        
     return render_template("index.html")
 
 @app.route("/link", methods=["GET", "POST"])
 def links():
+    
     if request.method == "POST":
         link_encurtado = request.form.get("link")
         link = pyshorteners.Shortener().tinyurl.short(link_encurtado)
+
         return "Seu link é: " + link
+        
+    
     return render_template("encurtador.html")
 
 if __name__ == "__main__":
-    app.run(port="1100",debug=True)
+    app.run(port="2200",debug=True)
